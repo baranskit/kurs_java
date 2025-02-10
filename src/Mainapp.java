76ab940cd93c6b646dbb989672bce915d36964b5
Tomasz.Baranski <tomasz.baranski.freelancer@iyuno-sdi.com>
2025-02-10 22:15:08 +0100

    Updated structure

---
+++ src/Mainapp.java
@@ -0,0 +1,21 @@
+public class Mainapp {
+    public static void main(String[] args) {
+        int number = 10;
+        System.out.println(number);
+
+        double doubleNumber = 10.1;
+        System.out.println(doubleNumber);
+
+        String text = "text w Java";
+        System.out.println(text);
+
+        char character = 's';
+        System.out.println(character);
+
+        boolean trueValue = true;
+        System.out.println(trueValue);
+
+        boolean falseValue = false;
+        System.out.println(falseValue);
+    }
+}
